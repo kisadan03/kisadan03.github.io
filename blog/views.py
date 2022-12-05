18dd483fa4b0e2b4b105f5c0b7d73661dc81284f
minyoung123 <mmypark20@gmail.com>
2022-12-05 20:05:27 +0900

    파이썬 애니웨어

--- blog/views.py
+++ blog/views.py
@@ -40,11 +40,11 @@ def new_comment(request, pk):
         raise PermissionDenied
 
 
-def delete_comment(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    post = comment.post
-    if request.user.is_authenticated and request.user == comment.author:
-        comment.delete()
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post = post.post
+    if request.user.is_authenticated and request.user == post.author:
+        post.delete()
         return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
